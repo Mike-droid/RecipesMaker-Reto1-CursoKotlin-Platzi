671aeeb001fec0c4ecd552c8330134c40270fe6f
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2020-08-03 20:15:23 -0500

    Reto 2 de kotlin completado

--- src/Menu.kt
+++ src/Menu.kt
@@ -6,7 +6,6 @@ fun main() {
         opcion = readLine()
         leerOpcion(opcion.toString())
     } while(!opcion.equals("3"))
-
 }
 
 fun mostrarMenu(){
@@ -21,8 +20,10 @@ fun mostrarMenu(){
 }
 
 fun leerOpcion(opcion:String){
-    when(opcion){
-        "1" -> println("""
+    var escogeReceta:String? = null
+
+    if(opcion=="1") {
+        println("""
             Tenemos los siguientes ingredientes:
             1.Agua
             2.Leche
@@ -32,13 +33,36 @@ fun leerOpcion(opcion:String){
             6.Verduras
             7.Cereal
             8.Frutas
-        """.trimIndent())
-        "2" -> println("""
-            Tus recetas son:
-            1) Huevos con aceite
-            2) Carne con verduras
-            3) Cereal con leche
+            ¿Con cuál deseas hacer una receta?
         """.trimIndent())
     }
+        else if (opcion == "2") {
+            println("""
+            Tenemos los siguientes ingredientes:
+            1.Agua
+            2.Leche
+            3.Carne
+            4.Huevos
+            5.Aceite
+            6.Verduras
+            7.Cereal
+            8.Frutas
+            ¿De qué ingredientes quieres ver las recetas?
+        """.trimIndent())
+        escogeReceta = readLine()
+        leerIngrediente(escogeReceta.toString())
+        }
+}
 
+fun leerIngrediente(ingrediente:String){
+    when(ingrediente){
+        "1" -> println("Recetas con agua: lorem")
+        "2" -> println("Recetas con leche: lorem")
+        "3" -> println("Recetas con carne: lorem")
+        "4" -> println("Recetas con huevos: lorem")
+        "5" -> println("Recetas con aceite: lorem")
+        "6" -> println("Recetas con verduras: lorem")
+        "7" -> println("Recetas con cereal: lorem")
+        "8" -> println("Recetas con frutas: lorem")
+    }
 }
